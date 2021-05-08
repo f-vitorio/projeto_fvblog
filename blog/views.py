b43cc2f51f4f5f74966e54e62503435d2abce5f0
fvitorio <fabio.vitorios@gmail.com>
2021-05-07 22:42:11 -0300

    Atualizado vies, templates, css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts= [
+    { 
+        'author': 'fvitorio',
+        'title': 'Blog Post1',
+        'content': 'primeiro conteudo',
+        'date_posted': 'august 27,2021'
+        },
+
+  { 
+        'author': 'vitorio',
+        'title': 'Blog Post2',
+        'content': ' conteudo',
+        'date_posted': 'august 27,2031'
+        },
+
+   ]
+
+
+
+
+def home(request):
+    context = {
+        'posts':posts
+            }
+    return render (request, 'blog/home.html',context)
+
+
+def about(request):
+    return render (request, 'blog/about.html')
