8438f0469eb47890959b54941a562f86f968adf1
fvitorio <fabio.vitorios@gmail.com>
2021-05-08 10:26:56 -0300

    atualizando models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    date_posted = models.DateTimeField(default=timezone)
+    
+
